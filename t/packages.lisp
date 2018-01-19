c6f66e4151a78c3261e2ef7afaf72f532042da41
Juan Reynoso Elias <juanreynoso@ikkiware.com>
2018-01-19 17:04:46 -0600

    Fixed minor error in t/packages.lisp

--- t/packages.lisp
+++ t/packages.lisp
@@ -8,6 +8,10 @@
 	:it.bese.FiveAM))
 
 (unless (5am:get-test :it.bese)
+  ;; define the suite
   (5am:def-suite :it.bese))
+;;if exists the suite called :it.bese
+(when (5am:get-test :it.bese)
+  (5am:def-suite :it.bese.yaclml :in :it.bese))
 
-(5am:def-suite :it.bese.yaclml :in :it.bese)
+
